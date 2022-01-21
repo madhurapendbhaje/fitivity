0c7102bdd11de144e7d186082c1564ca0972e82a
Madhura Pendbhaje <madhurapendbhaje22@gmail.com>
2022-01-20 21:03:29 -0800

    Server boilerplate

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const challengesRoutes = require("./routes/challenges");
+
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+
+// Enable CORS
+app.use(cors());
+
+app.use("/challenges", challengesRoutes);
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});

71555d609633836dee6dee4ee1e560804ce44d98
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-21 09:33:39 -0500

    created work-out report page folder

---
+++ client/src/Pages/PersonalChalReport/PersonalChalReport.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./PersonalChalReport.scss";
+
+export default class PersonalChalReport extends Component {
+  render() {
+    return <div></div>;
+  }
+}

c3dd8ac4ef3fd0181ff09ee2a4938134c9eb2b96
Ian Virtue <ianvirtue101@gmail.com>
2022-01-21 09:33:04 -0500

    deleted duplicate package files

---
+++ client/src/Pages/PersonalChalList/PersonalChalList.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./PersonalChalList.scss";
+
+export default class PersonalChal extends Component {
+  render() {
+    return <div>PersonalChal test</div>;
+  }
+}

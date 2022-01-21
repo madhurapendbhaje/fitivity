cbf2aa704395d7ba6ee0cd80b5d0c4d9ff5038a8
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-20 23:12:20 -0500

    set up routes on app.js and created pages folder

---
+++ client/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./HomePage.scss";
+
+export default class HomePage extends Component {
+  render() {
+    return <div>Home Page Test</div>;
+  }
+}
---
+++ client/src/Pages/PersonalChal/PersonalChal.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./PersonalChal.scss";
+
+export default class PersonalChal extends Component {
+  render() {
+    return <div>PersonalChal test</div>;
+  }
+}

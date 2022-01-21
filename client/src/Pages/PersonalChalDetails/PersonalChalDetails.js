315435d00a9a12dd1fa47c37e2c40131b8552835
Gloria Kuang <gloria.kcm@gmail.com>
2022-01-21 01:25:14 -0500

    finished video and header styling on challenge details page

--- client/src/Pages/PersonalChalDetails/PersonalChalDetails.js
+++ client/src/Pages/PersonalChalDetails/PersonalChalDetails.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
 import "./PersonalChalDetails.scss";
-import videoPoster from "../../assets/video-placeholder.jpeg";
+import videoPoster from "../../styles/assets/image/video-placeholder.jpeg";
 
 export default class PersonalChalDetails extends Component {
   render() {
     return (
       <div>
         <video loop autoPlay className="challenge-video" poster={videoPoster}>
-          <source src="" type="video/mp4" />
+          {/* <source src="" type="video/mp4" /> */}
         </video>
-        <div>
-          <img></img>
-          <h3>Challenge Name</h3>
-          <p>Beginner</p>
+        <div className="challenge-header">
+          <div className="challenge-header__image" alt="header"></div>
+          <h3 className="challenge-header__title">Challenge Name</h3>
+          <p className="challenge-header__level">Beginner</p>
         </div>
         <div className="challenge-details">
           <div className="challenge-details__about">
-            <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <h3 className="challenge-details__about-title">About</h3>
+            <p className="challenge-details__about-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+            </p>
           </div>
           <div className="challenge-focus">
             <h3>Areas of Focus</h3>
@@ -28,7 +30,7 @@ export default class PersonalChalDetails extends Component {
             <div></div>
             <p>Sleep</p>
           </div>
-          <div className="challenge-">
+          <div className="challenge-tasks">
             <h3>7-Day Challenge</h3>
             <article>
               <img></img>
